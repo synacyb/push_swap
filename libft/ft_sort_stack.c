977847724458436f5e736d2e90a6f2015bf7c7c7
Ayoub Adouay <ayadouay@c4r8p5.1337.ma>
2025-01-23 10:48:45 +0100

    V-3

--- libft/ft_sort_stack.c
+++ libft/ft_sort_stack.c
@@ -1,26 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_stack.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 10:45:35 by ayadouay          #+#    #+#             */
+/*   Updated: 2025/01/23 10:48:05 by ayadouay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	get_range(t_list *stack)
 {
-	int len;
+	int	len;
 
 	len = ft_lstsize(stack);
 	if (len <= 16)
 		return (len / 2);
-	else if(len <= 100)
+	else if (len <= 100)
 		return (len / 5);
 	else if (len <= 500)
 		return (len / 12);
 	else
 		return (len / (len / 2));
 }
+
 int	*creat_arr(t_list *list)
 {
-	d_sort my_arr;
+	d_sort	my_arr;
+
 	my_arr.len = ft_lstsize(list);
 	my_arr.arr = malloc(my_arr.len * sizeof(int));
 	if (!(my_arr.arr))
-		return 0;
+		return (0);
 	my_arr.index = 0;
 	while (list)
 	{
@@ -28,7 +42,7 @@ int	*creat_arr(t_list *list)
 		list = list->next;
 		my_arr.index++;
 	}
-	return my_arr.arr;
+	return (my_arr.arr);
 }
 
 void	ft_check_stack_b(t_list **stack_b)
@@ -40,15 +54,20 @@ void	ft_check_stack_b(t_list **stack_b)
 	}
 }
 
-void    ft_sort_stack(t_list **stack_a, t_list **stack_b)
+void	helper(d_sort *my_data, t_list **stack_a)
 {
-	d_sort my_data;
+	my_data->range = get_range(*stack_a);
+	my_data->arr = creat_arr(*stack_a);
+	my_data->len = ft_lstsize(*stack_a);
+	my_data->index = 0;
+	ft_insertion_sort(my_data->arr, ft_lstsize(*stack_a));
+}
 
-	my_data.range = get_range(*stack_a);
-	my_data.arr = creat_arr(*stack_a);
-	my_data.len = ft_lstsize(*stack_a);
-	my_data.index = 0;
-	ft_insertion_sort(my_data.arr, ft_lstsize(*stack_a));
+void	ft_sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	d_sort	my_data;
+
+	helper(&my_data, stack_a);
 	while (*stack_a)
 	{
 		if (my_data.range + my_data.index >= my_data.len)
